5a6e60f2bb77b4b2fa7d36c022f7055373c20768
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2024-01-18 19:16:52 +0100

    Fixes hawtkeys binds

--- .config/nvim/lua/plugins/hawtkeys.lua
+++ .config/nvim/lua/plugins/hawtkeys.lua
@@ -6,4 +6,13 @@ local Plugin = {
 function Plugin.opts()
 end
 
+function Plugin.config()
+    local hawtkeys = require('hawtkeys')
+    hawtkeys.setup {}
+
+    vim.keymap.set("n", "<leader>hk", ":Hawtkeys<CR>")
+    vim.keymap.set("n", "<leader>hkk", ":HawtkeysAll<CR>")
+    vim.keymap.set("n", "<leader>hkd", ":HawtkeysDupes<CR>")
+end
+
 return Plugin
