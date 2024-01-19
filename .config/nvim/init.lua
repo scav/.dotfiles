0f6e2b2c3276c23edd95fd4c9c47374077941a05
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2024-01-19 11:45:46 +0100

    Adds colour to line numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,3 +9,8 @@ load('user.keymaps')
 require('user.plugins')
 
 pcall(vim.cmd.colorscheme, 'tokyonight')
+
+-- Add more colours to the line numbers
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
+vim.api.nvim_set_hl(0, 'LineNr', { fg = '#FB508F', bold = true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#51B3EC', bold = true })
