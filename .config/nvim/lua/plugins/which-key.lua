d1ec1e272322a51882fcf182d9a567962ba8b31d
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2024-12-09 09:49:17 +0100

    addds which-key again

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,16 @@
+local Plugin = {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+}
+
+return Plugin
