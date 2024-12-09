7e380662755b367274bc47a38b9aebbb7577a9ea
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2024-12-09 12:54:27 +0100

    cleanup, remove noice, add snacks

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,15 +3,8 @@ local Plugins = {
     { 'tpope/vim-repeat' },
     { 'nvim-tree/nvim-web-devicons', lazy = true },
 
-    -- Themes
+    -- Theme
     { 'folke/tokyonight.nvim' },
-    --  {'joshdick/onedark.vim'},
-    --  {'tanvirtin/monokai.nvim'},
-    --  {'lunarvim/darkplus.nvim'},
-    --  { 'rose-pine/neovim',               name = 'rose-pine' },
-    --  { 'scottmckendry/cyberdream.nvim' },
-    --  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 }
-    --  { 'rmehri01/onenord.nvim' },
 }
 
 return Plugins
---
+++ .config/nvim/lua/plugins/snacks-nvim.lua
@@ -0,0 +1,144 @@
+local Plugin = {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        quickfile = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+        bigfile = { enabled = true },
+        notifier =
+        ---@class snacks.notifier.Config
+        ---@field keep? fun(notif: snacks.notifier.Notif): boolean # global keep function
+        {
+            timeout = 3000, -- default timeout in ms
+            width = { min = 40, max = 0.4 },
+            height = { min = 1, max = 0.6 },
+            -- editor margin to keep free. tabline and statusline are taken into account automatically
+            margin = { top = 0, right = 1, bottom = 0 },
+            padding = true,              -- add 1 cell of left/right padding to the notification window
+            sort = { "level", "added" }, -- sort by level and time
+            -- minimum log level to display. TRACE is the lowest
+            -- all notifications are stored in history
+            level = vim.log.levels.TRACE,
+            icons = {
+                error = " ",
+                warn = " ",
+                info = " ",
+                debug = " ",
+                trace = " ",
+            },
+            keep = function(notif)
+                return vim.fn.getcmdpos() > 0
+            end,
+            ---@type snacks.notifier.style
+            style = "compact",
+            top_down = true,    -- place notifications from top to bottom
+            date_format = "%R", -- time format for notifications
+            -- format for footer when more lines are available
+            -- `%d` is replaced with the number of lines.
+            -- only works for styles with a border
+            ---@type string|boolean
+            more_format = " ↓ %d lines ",
+            refresh = 50, -- refresh at most every 50ms
+        },
+        dashboard =
+        ---@class snacks.dashboard.Config
+        ---@field sections snacks.dashboard.Section
+        ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
+        {
+            preset = {
+                header = { 'foo' },
+                keys = {
+                    { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                    { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                    { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                    { icon = " ", key = "c", desc = "Mason", action = ":Mason" },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+            },
+            sections = {
+                {
+                    pane = 1,
+                    desc = "Shortcuts",
+                    height = 1,
+                    padding = 1,
+                },
+                {
+                    pane = 2,
+                    desc = "GitHub",
+                    height = 1,
+                    padding = 1,
+                },
+                { section = "keys",    gap = 1,     padding = 1 },
+                {
+                    pane = 2,
+                    icon = " ",
+                    desc = "Browse Repo",
+                    padding = 1,
+                    key = "b",
+                    action = function()
+                        Snacks.gitbrowse()
+                    end,
+                },
+                function()
+                    local in_git = Snacks.git.get_root() ~= nil
+                    local cmds = {
+                        {
+                            title = "Notifications",
+                            cmd = "gh notify -s -a -n5",
+                            action = function()
+                                vim.ui.open("https://github.com/notifications")
+                            end,
+                            key = "n",
+                            icon = " ",
+                            height = 5,
+                            enabled = true,
+                        },
+                        {
+                            title = "Open Issues",
+                            cmd = "gh issue list -L 3",
+                            key = "i",
+                            action = function()
+                                vim.fn.jobstart("gh issue list --web", { detach = true })
+                            end,
+                            icon = " ",
+                            height = 7,
+                        },
+                        {
+                            icon = " ",
+                            title = "Open PRs",
+                            cmd = "gh pr list -L 3",
+                            key = "p",
+                            action = function()
+                                vim.fn.jobstart("gh pr list --web", { detach = true })
+                            end,
+                            height = 7,
+                        },
+                        -- {
+                        --     icon = " ",
+                        --     title = "Git Status",
+                        --     cmd = "hub --no-pager diff --stat -B -M -C",
+                        --     height = 10,
+                        -- },
+                    }
+                    return vim.tbl_map(function(cmd)
+                        return vim.tbl_extend("force", {
+                            pane = 2,
+                            section = "terminal",
+                            enabled = in_git,
+                            padding = 1,
+                            ttl = 5 * 60,
+                            indent = 3,
+                        }, cmd)
+                    end, cmds)
+                end,
+                { section = "startup", padding = 10 },
+            },
+        }
+
+    }
+}
+return Plugin
