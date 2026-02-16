9de1ea2da7ea3dc8d1b81fef152b56b5c28f0027
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2026-02-16 10:39:19 +0100

    wrk: add google-cloud-sdk and gke-gcloud-auth-plugin

--- nix-systems/wrk/home.nix
+++ nix-systems/wrk/home.nix
@@ -1,4 +1,8 @@
 { config, pkgs, ... }:
 {
   home.stateVersion = "26.05";
+
+  home.packages = with pkgs; [
+    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+  ];
 }
