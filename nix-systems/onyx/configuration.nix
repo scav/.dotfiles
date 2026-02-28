881daad4e5c1385fb47a90b6acbfd2cd5440d937
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2026-02-28 01:09:30 +0100

    onyx: adds gamescope to steam

--- nix-systems/onyx/configuration.nix
+++ nix-systems/onyx/configuration.nix
@@ -81,6 +81,7 @@
 
   programs.steam = {
     enable = true;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
