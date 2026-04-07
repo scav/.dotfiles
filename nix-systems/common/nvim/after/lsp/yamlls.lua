e78a966743cfa184c85fb9f2154aab1de119ccfe
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2026-04-07 10:50:19 +0200

    nvim: fixes yamlls

--- nix-systems/common/nvim/after/lsp/yamlls.lua
+++ nix-systems/common/nvim/after/lsp/yamlls.lua
@@ -1,10 +1,12 @@
-vim.lsp.config("yamlls", {
-	settings = {
-		yaml = {
-			schemas = {
-				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.32.1-standalone-strict/all.json"] = "/*.k8s.yaml",
-				..., -- other schemas
+return {
+	vim.lsp.config("yamlls", {
+		settings = {
+			yaml = {
+				schemas = {
+					["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.32.1-standalone-strict/all.json"] = "/*.k8s.yaml",
+					..., -- other schemas
+				},
 			},
 		},
-	},
-})
+	}),
+}
