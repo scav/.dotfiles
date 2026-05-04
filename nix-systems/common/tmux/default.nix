437f3de78e53c75b7bee7c6a7be24435d62f77e9
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2026-05-04 11:20:17 +0200

    common: split tmux.conf into separate files

--- nix-systems/common/tmux/default.nix
+++ nix-systems/common/tmux/default.nix
@@ -25,18 +25,7 @@
       tmux-fzf
       {
         plugin = tokyo-night-tmux;
-        extraConfig = ''
-          set -g @tokyo-night-tmux_theme storm
-          set -g @tokyo-night-tmux_transparent 1
-          set -g @tokyo-night-tmux_show_wbg 0
-          set -g @tokyo-night-tmux_show_netspeed 0
-          set -g @tokyo-night-tmux_netspeed_showip 0
-          set -g @tokyo-night-tmux_netspeed_show 0
-          set -g @tokyo-night-tmux_show_git 0
-          set -g @tokyo-night-tmux_show_datetime 0
-          set -g @tokyo-night-tmux_date_format MYD
-          set -g @tokyo-night-tmux_time_format 12H
-        '';
+        extraConfig = builtins.toString (builtins.readFile ./tmux-tokyonight.conf); # a // b to combine files
       }
     ];
 
