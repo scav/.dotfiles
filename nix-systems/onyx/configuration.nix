6c5e8d40cf93bdcff3e2c1f3d198ba3d7e721d2e
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2026-07-15 00:35:50 +0200

    onyx: extends virtualisation to support tpm

--- nix-systems/onyx/configuration.nix
+++ nix-systems/onyx/configuration.nix
@@ -147,7 +147,14 @@
   programs.virt-manager.enable = true;
 
   virtualisation = {
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+      };
+    };
     containers.enable = true;
     podman = {
       enable = true;
