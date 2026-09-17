0f2c91cb6e6037e640a60d4fbe411938ff842b34
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2026-09-17 10:46:15 +0200

    thinkpad: adds work monitor to hyprland

--- nix-systems/thinkpad/modules/hypr/monitors.lua
+++ nix-systems/thinkpad/modules/hypr/monitors.lua
@@ -1,13 +1,22 @@
+-- Built in
 hl.monitor({
-	output = "",
-	mode = "3200x2000@120.00",
-	position = "auto",
-	scale = "1.0",
+    output = "",
+    mode = "3200x2000@120.00",
+    position = "auto",
+    scale = "1.0",
+})
+-- Home
+hl.monitor({
+    output = "desc: Samsung Electric Company LS49AG95 HNTTA00056",
+    mode = "5120x1440@240",
+    position = "auto",
+    scale = "1.0",
 })
 
+-- Work
 hl.monitor({
-	output = "desc: Samsung Electric Company LS49AG95 HNTTA00056",
-	mode = "5120x1440@240",
-	position = "auto",
-	scale = "1.0",
+    output = "desc: HP Inc. HP E34m G4 CNC4413HB",
+    mode = "3440x1440@75",
+    position = "auto",
+    scale = "1.0",
 })
