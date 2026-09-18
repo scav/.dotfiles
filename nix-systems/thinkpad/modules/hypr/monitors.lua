6c337415a6aa651a2e8cc5b0d70aa9d1abfdc3ee
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2026-09-18 18:41:13 +0200

    thinkpad: sets up monitors for hyprland

--- nix-systems/thinkpad/modules/hypr/monitors.lua
+++ nix-systems/thinkpad/modules/hypr/monitors.lua
@@ -1,22 +1,25 @@
+-- Use integrated GPU as primary renderer
+hl.env("AQ_DRM_DEVICES", "/dev/dri/card0:/dev/dri/card1")
+
 -- Built in
 hl.monitor({
-    output = "",
-    mode = "3200x2000@120.00",
-    position = "auto",
-    scale = "1.0",
+	output = "",
+	mode = "3200x2000@120.00",
+	position = "auto-right",
+	scale = "1.6",
 })
 -- Home
 hl.monitor({
-    output = "desc: Samsung Electric Company LS49AG95 HNTTA00056",
-    mode = "5120x1440@240",
-    position = "auto",
-    scale = "1.0",
+	output = "desc: Samsung Electric Company LS49AG95 HNTTA00056",
+	mode = "5120x1440@240",
+	position = "auto",
+	scale = "1.0",
 })
 
 -- Work
 hl.monitor({
-    output = "desc: HP Inc. HP E34m G4 CNC4413HB",
-    mode = "3440x1440@75",
-    position = "auto",
-    scale = "1.0",
+	output = "desc: HP Inc. HP E34m G4 CNC4413HB",
+	mode = "3440x1440@75",
+	position = "auto",
+	scale = "1.0",
 })
