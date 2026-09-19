1e1e54092640ba7528cc1d867b16fcbf7060ee52
Dag Østgulen Heradstveit <190020+scav@users.noreply.github.com>
2026-09-19 11:50:08 +0200

    common-linux: adds mute support to wob

--- nix-systems/common-linux/hypr/lua/binds.lua
+++ nix-systems/common-linux/hypr/lua/binds.lua
@@ -54,7 +54,11 @@ hl.bind(
 )
 hl.bind(
     "XF86AudioMute",
-    hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+    hl.dsp.exec_cmd([[
+        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && \
+        (wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q MUTED && echo 0 > $XDG_RUNTIME_DIR/wob.sock) || \
+        wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed 's/[^0-9]//g' > $XDG_RUNTIME_DIR/wob.sock
+        ]]),
     { locked = true, repeating = true }
 )
 
